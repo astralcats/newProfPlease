4d6c3267de1b2041bd6dc9c3c77505c8f077472a
piotrflorczak12 <piotr.florczak12@gmail.com>
2022-11-26 21:42:51 +0100

    first commit

---
+++ .history/src/Pages/Dashboard/Mint_20221126121001.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./styles/Mint.css";
+import AnimatedPage from "../../Components/AnimatedPage";
+// import prof from "../../Assets/prof.png";
+import thread from "../../Assets/thread.png";
+import text from "../../Assets/professor-text.png";
+import board from "../../Assets/board.png";
+import arrow from "../../Assets/arrow.png";
+import prof from "../../Assets/prof.gif";
+import gif from "../../Assets/gif.gif";
+
+const Mint = () => {
+  return (
+    <AnimatedPage>
+      <div className="mint-page">
+        <img src={prof} className="prof" alt="" />
+        <div className="gif-div">
+          <img src={thread} className="wire" alt="" />
+          <img src={gif} className="gif" alt="" />
+          <p className="cp">
+            !!! NO CODE !!! CLICK THE ALPHA BUTTON TO GET ONE FREE !!!!!
+          </p>
+        </div>
+        <div className="mint-div">
+          <div className="arrow-div">
+            <div className="arrow-line"></div>
+            <div className="arrow-head"></div>
+          </div>
+          <div to={"/dashboard"} className="mint-btn">
+            <img src={board} alt="" />
+            <p className="btn-text cp">Mint Professor</p>
+          </div>
+          <div className="amount-div">
+            <h2 className="cp">1/10,000</h2>
+            <input className="cp" type="text" />
+          </div>
+          <div className="input-desc">
+            <p className="cp">INPUT MINT CODE FROM TG GROUP HERE</p>
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+        <div className="list-text">
+          <h3 className="cp">FREE MINT FEATURES</h3>
+          <ul>
+            <li className="cp"> 10,000 Unique Professors</li>
+            <li className="cp">
+              Free Access To 'LABZ' Tech Builds In Professor AMA Lounge
+            </li>
+            <li className="cp">Whitelist Spot For Wagmilabz Collections</li>
+            <li className="cp">Wagmi Market place access</li>
+          </ul>
+        </div>
+        {/* <img src={prof} alt="" className="prof" /> */}
+        <img src={text} alt="" className="prof-text" />
+      </div>
+    </AnimatedPage>
+  );
+};
+
+export default Mint;
